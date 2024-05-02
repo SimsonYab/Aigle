fe75113da7a79ef8361c22c01d988997c5dae8d0
Siméon YABRE <simson.yabre@gmail.com>
2024-05-02 00:59:21 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'digitapp',
+  webDir: 'www'
+};
+
+export default config;
